cbdcc2ba2f761ec4ebdba9ff5e223d495fe64305
PATHMANANTHAN <50802779+PATHMANANTHAN@users.noreply.github.com>
2022-09-07 17:56:44 +0530

    Add files via upload

---
+++ Webcreate-1/src/main/java/com/babyweb/Webcreate1Application.java
@@ -0,0 +1,13 @@
+package com.babyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webcreate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webcreate1Application.class, args);
+	}
+
+}
---
+++ Webcreate-1/src/test/java/com/babyweb/Webcreate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webcreate1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
